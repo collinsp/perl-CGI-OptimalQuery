db589050b5a162f7cc184d647bdd108dbde8db60
Phil Collins <Philip.Collins@unh.edu>
2014-05-07 07:25:15 -0400

    moved export menu back to originally location; tweeks to close button; fix manifest;

--- lib/CGI/OptimalQuery/InteractiveQuery2.pm
+++ lib/CGI/OptimalQuery/InteractiveQuery2.pm
@@ -116,6 +116,25 @@ $script
 </script><noscript>Javascript is required when viewing this page.</noscript>";
     $buf .= "
 <div class=OQdocTop>$opts{OQdocTop}</div>";
+
+    # ouput tools panel
+    my @tools = sort keys %{$$o{schema}{tools}};
+    if ($#tools > -1) {
+      $buf .= "<div class=OQToolsPanel-pos-div><div class=OQToolsPanel-align-div><div class=OQToolsPanel><ul>";
+      my $opened_tool_key = $$o{q}->param('tool');
+      foreach my $key (sort keys %{$$o{schema}{tools}}) {
+        my $tool = $$o{schema}{tools}{$key};
+
+        my $openedClass = '';
+        my $toolContent = '';
+        if ($opened_tool_key eq $key) {
+          $openedClass = ' opened';
+          $toolContent = "<div class=OQToolContent>".$$tool{handler}->($o)."</div>";
+        }
+        $buf .= "<li data-toolkey='$key' class='OQToolExpander $openedClass'><h3>".escapeHTML($$tool{title})."</h3>$toolContent</li>";
+      }
+      $buf .= "</ul><button class=OQToolsCancelBut type=button>&#10005;</button></div></div></div>";
+    }
   }
   $buf .= "
 <form class=OQform name=OQform action='".escapeHTML($$o{schema}{URI_standalone}||$$o{schema}{URI})."' method=get>
@@ -298,25 +317,6 @@ $newBut
   if ($dataonly) {
     $buf .= "</body></html>";
   } else {
-
-    # ouput tools panel
-    my @tools = sort keys %{$$o{schema}{tools}};
-    if ($#tools > -1) {
-      $buf .= "<div class=OQToolsPanel><ul>";
-      my $opened_tool_key = $$o{q}->param('tool');
-      foreach my $key (sort keys %{$$o{schema}{tools}}) {
-        my $tool = $$o{schema}{tools}{$key};
-
-        my $openedClass = '';
-        my $toolContent = '';
-        if ($opened_tool_key eq $key) {
-          $openedClass = ' opened';
-          $toolContent = "<div class=OQToolContent>".$$tool{handler}->($o)."</div>";
-        }
-        $buf .= "<li data-toolkey='$key' class='OQToolExpander $openedClass'><h3>".escapeHTML($$tool{title})."</h3>$toolContent</li>";
-      }
-      $buf .= "</ul><button class=OQToolsCancelBut type=button>&#10005;</button></div>";
-    }
 
     $buf .= "<div class=OQdocBottom>$opts{OQdocBottom}</div>";
     #$buf .= "<a href=# style='color: #999; float: right;' onclick=\"document.cookie='OQIQ2=;path=/;expires=Thu, 01-Jan-1970 00:00:01 GMT';window.location.reload(true);return false;\">classic mode</a>" if $ENV{HTTP_COOKIE} =~ /\bOQIQ2\=1\b/;

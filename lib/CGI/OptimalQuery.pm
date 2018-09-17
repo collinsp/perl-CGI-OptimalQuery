220ebe3e200a849e7b4b5e737597d60b46095643
Philip Collins <Philip.Collins@unh.edu>
2018-09-17 14:21:42 -0400

    release .31

--- lib/CGI/OptimalQuery.pm
+++ lib/CGI/OptimalQuery.pm
@@ -9,7 +9,7 @@ use CGI::OptimalQuery::SavedSearches();
 BEGIN {
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.30';
+    $VERSION     = '0.31';
     @ISA         = qw(Exporter);
     #Give a hoot don't pollute, do not export more than needed by default
     @EXPORT      = qw();
